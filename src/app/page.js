f6b9f0e106535da06592abe54ed290225475d989
prajaktpatilaweb <prajaktaweb@gmail.com>
2024-07-15 16:51:40 +0530

    change in page file

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,32 @@
-import Features from "@components/components/Features";
+// import Features from "@components/components/Features";
 import Footer from "@components/components/Footer";
-import Hero from "@components/components/Hero";
-import Highlights from "@components/components/Highlights";
-import HowItWorks from "@components/components/HowItWorks";
-import Model from "@components/components/Model";
+// import Hero from "@components/components/Hero";
+// import Highlights from "@components/components/Highlights";
+// import HowItWorks from "@components/components/HowItWorks";
+// import Model from "@components/components/Model";
 import Navbar from "@components/components/Navbar";
-import Image from "next/image";
+import dynamic from 'next/dynamic';
+
+
+const Highlights = dynamic(() => import('../components/Highlights'), {
+  ssr: false
+});
+const Hero = dynamic(() => import('../components/Hero'), {
+  ssr: false
+});
+
+const Model = dynamic(() => import('../components/Model'), {
+  ssr: false
+});
+
+const Features = dynamic(() => import('../components/Features'), {
+  ssr: false
+});
+
+const HowItWorks = dynamic(() => import('../components/HowItWorks'), {
+  ssr: false
+});
+
 
 export default function Home() {
   return (
